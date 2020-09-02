d22c263321f6fa5f691a168a35e055453f3ea0bb
a-msy <m5s2y5a@gmail.com>
2020-09-03 00:59:12 +0900

    seeder変更

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class, AdminsTableSeeder::class, MastersTableSeeder::class, ArticleTableSeeder::class,TagsTableSeeder::class]);
+        $this->call([
+            UsersTableSeeder::class,
+            AdminsTableSeeder::class,
+            MastersTableSeeder::class,
+            ArticleTableSeeder::class,
+            TagsTableSeeder::class,
+            EventsTableSeeder::class,
+            HousesTableSeeder::class
+        ]);
     }
 }
