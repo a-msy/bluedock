b6c1de4515e8f651687f92f92ec16eefccd263f3
a-msy <m5s2y5a@gmail.com>
2020-09-19 22:35:30 +0900

    footer完成

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,22 @@
-<footer>
-    footer
+<footer class="background-black text-white py-5 font-size-14px">
+    <div class="container">
+        <p class="text-center">bluedock</p>
+        <div class="py-5 text-center">
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    お問い合わせ　｜
+                </li>
+                <li class="list-inline-item">
+                    運営者情報　｜
+                </li>
+                <li class="list-inline-item">
+                    利用規約　｜
+                </li>
+                <li class="list-inline-item">
+                    プライバシーポリシー
+                </li>
+            </ul>
+        </div>
+        <p class="text-center">Copyright 2020 Twenties Music Club.</p>
+    </div>
 </footer>
