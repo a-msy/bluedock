22c7731397d392c779ef45d804aeb14a1888f214
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2020-09-28 20:04:47 +0900

    OkiRyo designed.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.common')
 @section('content')
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+<div class="section-header text-center @isset($addcss) {{$addcss}} @endif" style="@isset($addstyle) {{$addstyle}} @endif">
+    <h1>アーティスト一覧</h1>
+    <div class="line" style="@isset($addstyle2) {{$addstyle2}} @endif"></div>
+</div>
+
     <div class="container">
-        <ul>
+        <ul style="width: 100%; list-style: none;">
             @foreach($admins as $admin)
-                <li><a href="{{route('admin.detail',['id'=>$admin->id])}}">{{$admin->name}}</a></li>
+                <li style="padding: 0; float: left; width:50%;">
+                    <div style="inline-block; text-align: center;">
+                        <img src="https://via.placeholder.com/200" class="object-fit pictures"></img>
+                        <br>
+                        <a href="{{route('admin.detail',['id'=>$admin->id])}}" style="font-size: 2vw; line-height: 150%;">{{$admin->name}}</a>
+                    </div>
+                    <div class="tag">
+                        Tag
+                    </div>
+                </li>
             @endforeach
         </ul>
         {{$admins->links()}}
--- resources/views/article/detail.blade.php
+++ resources/views/article/detail.blade.php
@@ -1,10 +1,58 @@
 @extends('layouts.common')
 @section('content')
-    <div class="container">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+<div class="thumbnail position-relative">
+    <img src="{{asset('storage/img/article_pictures/'.$article->eyecatch)}}" class="object-fit"/>
+    <div class="position-absolute" style="bottom:0px; z-index:99; margin-left: 1%">
         <h1>{{$article->title}}</h1>
-        <div>
-            {!! $article->body !!}
-        </div>
-        <img src="{{asset('storage/img/article_pictures/'.$article->eyecatch)}}">
     </div>
+</div>
+
+<div class="container" style="font-size: 1vw; margin-top: 10px">
+    <div style="font-size: 15px">
+        <span style="color:#00bfff">NEWS</span><span style="color: #c0c0c0">|2020.09.15</span>
+    </div>
+    <div style="line-height: 30px; font-size: 1vw;">
+        <figure class="float-right">
+            <img src="http://via.placeholder.com/150x100">
+            <figcaption style="color: grey;">
+                TEST
+            </figcaption>
+            <figcaption class="zoom float-right">
+                拡大
+            </figcaption>
+        </figure>
+        {!! $article->body !!}
+    </div>
+</div>
+
+<div class="section-header text-center @isset($addcss) {{$addcss}} @endif" style="@isset($addstyle) {{$addstyle}} @endif"></div>
+<br/>
+<div　class="tag">
+    神宿
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="titlebar col-8">
+            PICTURE
+        </div>
+    </div>
+    <button> More</button>
+    <br/>
+    <br/>
+    <div class="row">
+        <div class="titlebar col-8">
+            神宿
+        </diV>
+    </div>
+</div>
+@include('layouts.section-header',['title'=>'INTERVIEW',])
+<section class="background-black overflow-hidden popular">
+    {{--        TODO::人気順記事を取得するロジックを組む--}}
+    @include('layouts.section-header',['title'=>'POPULAR','addcss'=>"text-white",'addstyle'=>"",'addstyle2'=>"border-color:white !important;"])
+</section>
+@include('layouts.section-header',['title'=>'Pics Ranking',])
 @endsection
